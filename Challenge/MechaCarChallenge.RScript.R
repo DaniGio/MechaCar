c62936051ec37c628077d8707a708b807c5999ed
DaniGio <59997215+DaniGio@users.noreply.github.com>
2020-04-25 22:19:51 +1000

    deleting old files

--- Challenge/MechaCarChallenge.RScript.R
+++
@@ -1,4 +0,0 @@
-MechaCar_table <- read.csv(file='MechaCar_mpg.csv',sep=",", header = T)
-
-
-#lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + hp,data=MechaCar_table) #generate multiple linear regression model
